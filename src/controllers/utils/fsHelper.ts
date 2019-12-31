be02ef0b3aa1bc445a2c682793be7f21e6f7b717
naitianliu <naitianliu@gmail.com>
2019-12-31 12:16:35 -0800

    controllers - utils - fsHelper.ts

---
+++ src/controllers/utils/fsHelper.ts
@@ -0,0 +1,40 @@
+// controllers/utils/fsHelper.ts
+
+const fs = window.require('fs');
+const homedir = window.require('os').homedir();
+
+export enum ErrorType {
+    DirExists,
+    DirNotExists,
+
+}
+
+export class FSHelper {
+    createDirByPath = (path: string) => {
+        return new Promise((resolve, reject) => {
+            if (fs.existsSync(path)) {
+                reject(ErrorType.DirExists);
+            } else {
+                fs.mkdirSync(path);
+                resolve();
+            }
+        });
+    };
+
+    checkPathExists = (path: string) => {
+        return new Promise((resolve, reject) => {
+            fs.stat(path, (err: object, stats: object) => {
+                if (!err) resolve();
+                else reject(ErrorType.DirNotExists);
+            });
+        });
+    };
+
+    getDefaultPath = () => {
+        return `${homedir}/Flint/untitled`;
+    };
+
+    getFileTree = (rootDirPath: string) => {
+
+    };
+}
