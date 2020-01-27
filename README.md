f60afd88a80d99851ff38cf7c45d53ab57c751ab
naitianliu <naitianliu@gmail.com>
2020-01-27 09:24:48 -0800

    components - DialogForm.tsx - define props

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
 # flint
+
+* a
+* b
+* c
+
+## subtitle1
+
+- [x] to-do 1
+- [ ] to-do 2
+- [ ] to-do 3
+
+[google homepage](wwww.google.com)
+
+![cat](https://icatcare.org/app/uploads/2018/07/Thinking-of-getting-a-cat.png)
+
+| *name* | title | gender |
+|---|---|---|
+| vincent | baby | male |
+| ali | mom | female |
--- src/components/DialogForm/DialogForm.tsx
+++ src/components/DialogForm/DialogForm.tsx
@@ -6,6 +6,10 @@ import Dialog from "@material-ui/core/Dialog";
 import DialogTitle from "@material-ui/core/DialogTitle";
 import DialogContent from "@material-ui/core/DialogContent";
 import DialogActions from "@material-ui/core/DialogActions";
+import Button from '@material-ui/core/Button';
+import {Simulate} from "react-dom/test-utils";
+import submit = Simulate.submit;
+import {LOADING_STATUS} from "../../constants";
 
 const styles = createStyles({
     root: {
@@ -13,16 +17,47 @@ const styles = createStyles({
     },
 });
 
-export interface Props extends WithStyles<typeof styles>{
+type FormType = 'input' | 'select';
+
+interface Form {
+    type: FormType,
+    key: string,
+    label: string,
+    placeholder?: string,
+    helperText?: string,
+    required?: boolean,
+    defaultValue?: string|number,
+    options: Array<string|number>
+}
+
+interface Params {
+    [key: string]: string|number
+}
+
+interface Callback {
+    setStatus: (status: LOADING_STATUS) => void,
+    close: () => void,
+}
+
+export interface Props extends WithStyles<typeof styles> {
     open: boolean,
     onClose: () => void,
-    title: string,
+    title?: string,
+    submitButtonTitle?: string,
+    closeButtonTitle?: string,
+    forms: Form[],
+    onSubmit: (params: Params, callback: Callback) => void
+}
 
+interface State {
+    params: Params,
+    loadingStatus: LOADING_STATUS,
 }
 
 class DialogForm extends React.Component<Props, object> {
-    state = {
-
+    state: State = {
+        params: {},
+        loadingStatus: LOADING_STATUS.NOT_STARTED
     };
 
     componentDidMount(): void {
@@ -33,8 +68,19 @@ class DialogForm extends React.Component<Props, object> {
 
     };
 
+    handleSetStatus = (loadingStatus: LOADING_STATUS) => {
+        this.setState({loadingStatus});
+    };
+
+    handleSubmitButtonClick = () => {
+        const {params} = this.state;
+        const {onSubmit} = this.props;
+        const callback = {setStatus: this.handleSetStatus, close: this.props.onClose};
+        onSubmit(params, callback);
+    };
+
     render() {
-        const {classes, open, onClose} = this.props;
+        const {classes, open, onClose, title, submitButtonTitle, closeButtonTitle, forms} = this.props;
         return (
             <div className={classes.root}>
                 <Dialog
@@ -42,7 +88,26 @@ class DialogForm extends React.Component<Props, object> {
                     onClose={onClose}
                     onEnter={this.onEnter}
                 >
+                    {!!title &&
+                    <DialogTitle>{title}</DialogTitle>
+                    }
+                    <DialogContent>
 
+                    </DialogContent>
+                    <DialogActions>
+                        <Button
+                            onClick={onClose}
+                        >
+                            {!!closeButtonTitle ? closeButtonTitle : 'Close'}
+                        </Button>
+                        <Button
+                            variant={"contained"}
+                            color={"primary"}
+                            onClick={this.handleSubmitButtonClick}
+                        >
+                            {!!submitButtonTitle ? submitButtonTitle : 'Submit'}
+                        </Button>
+                    </DialogActions>
                 </Dialog>
             </div>
         )
