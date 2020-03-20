a392a72dc2a27c2917f7c8508024d8619c85e14d
naitianliu <naitianliu@gmail.com>
2020-03-20 15:40:53 -0700

    ui controller - source file generator - generate redux files

--- src/controllers/ui/sourceFileGenerator.ts
+++ src/controllers/ui/sourceFileGenerator.ts
@@ -7,9 +7,19 @@ import BabelRC from './templates/babelrc.txt';
 import IndexHTML from './templates/index-html.txt';
 import PackageJSON from './templates/package-json.txt';
 import WebpackConfig from './templates/webpack-config.txt';
+import ReduxStoreJS from './templates/redux-store-js.txt';
+import ReduxActionsJS from './templates/redux-actions-js.txt';
+import ReduxReducerJS from './templates/redux-reducer-js.txt';
+import ReduxTypesJS from './templates/redux-types-js.txt';
 import * as Mustache from "mustache";
 import * as _ from 'lodash';
 import {ProjectManager} from "../project/projectManager";
+import {StateUpdaterData} from "@flintdev/ui-editor/dist/interface";
+
+interface File {
+    path: string,
+    content: string,
+}
 
 export class SourceFileGenerator {
     uiDataManager: UIDataManager;
@@ -43,6 +53,7 @@ export class SourceFileGenerator {
         await this.checkAndCreateDir(this.sourceDirPath);
         await this.loadEditorData();
         await this.generateConfigFiles();
+        await this.generateReduxFiles();
     };
 
     private removeSourceDir = async () => {
@@ -50,7 +61,7 @@ export class SourceFileGenerator {
     };
 
     private generateConfigFiles = async () => {
-        let files: any[] = [];
+        let files: File[] = [];
         // webpack.config.js
         files.push({
             path: `${this.sourceDirPath}/webpack.config.js`,
@@ -74,7 +85,65 @@ export class SourceFileGenerator {
         await this.batchToCreateFiles(files);
     };
 
-    private batchToCreateFiles = async (files: any[]) => {
+    private generateComponentFiles = async () => {
+        let files: File[] = [];
+
+    };
+
+    private generateReduxFiles = async () => {
+        const {initialState, stateUpdaters} = this.editorData;
+        const reduxDir = `${this.sourceDirPath}/redux`;
+        await this.checkAndCreateDir(reduxDir);
+        let files: File[] = [];
+        const updaters = this.reformatUpdaters(stateUpdaters);
+        // redux actions.js
+        files.push({
+            path: `${reduxDir}/actions.js`,
+            content: Mustache.render(ReduxActionsJS, {updaters}),
+        });
+        // redux reducer.js
+        files.push({
+            path: `${reduxDir}/reducer.js`,
+            content: Mustache.render(ReduxReducerJS, {updaters}),
+        });
+        // redux types.js
+        files.push({
+            path: `${reduxDir}/types.js`,
+            content: Mustache.render(ReduxTypesJS, {updaters}),
+        });
+        // redux store.js
+        files.push({
+            path: `${reduxDir}/store.js`,
+            content: ReduxStoreJS,
+        });
+        // redux state.js
+        files.push({
+            path: `${reduxDir}/state.js`,
+            content: initialState,
+        });
+        await this.batchToCreateFiles(files);
+    };
+
+    private reformatUpdaters = (stateUpdaters: StateUpdaterData[]) => {
+        return stateUpdaters.map((updater: StateUpdaterData) => {
+            let data = {};
+            updater.operations.forEach(operation => {
+                const {field, operator, parameter} = operation;
+                const path = field.split('.').slice(1);
+                _.set(data, path, {[`$${operator.toLowerCase()}`]: `action.args.${parameter}`})
+            });
+            let dataStr = JSON.stringify(data, null, 4).replace(/"/gi, '');
+            const tempList = dataStr.split('\n');
+            dataStr = `${tempList.join('\n        ')}`;
+            return {
+                name: updater.name,
+                actionName: _.camelCase(updater.name),
+                data: dataStr,
+            }
+        })
+    };
+
+    private batchToCreateFiles = async (files: File[]) => {
         for (const file of files) {
             const {path, content} = file;
             await this.fsHelper.createFile(path, content);
