e063d9ab99bf8ded48fc5557dd605bb0fa0cd992
naitianliu <naitianliu@gmail.com>
2020-04-29 00:25:13 -0700

    support switch always and conditional radios to edit outputs

--- src/containers/editorWindow/MVCEditor/ProcessEditorView/StepEditDialog/StepOutputsPane/StepOutputsPane.tsx
+++ src/containers/editorWindow/MVCEditor/ProcessEditorView/StepEditDialog/StepOutputsPane/StepOutputsPane.tsx
@@ -1,10 +1,10 @@
 // src/containers/editorWindow/MVCEditor/ProcessEditorView/StepEditDialog/StepOutputsPane/StepOutputsPane.tsx
 
 import * as React from 'react';
-import { withStyles, WithStyles, createStyles } from '@material-ui/core/styles';
-import { connect } from 'react-redux';
-import { Dispatch } from "redux";
-import { StoreState } from "src/redux/state";
+import {withStyles, WithStyles, createStyles} from '@material-ui/core/styles';
+import {connect} from 'react-redux';
+import {Dispatch} from "redux";
+import {StoreState} from "src/redux/state";
 import * as actions from "src/redux/modules/editor/actions";
 import {themeColor} from "../../../../../../constants";
 import {Output, OutputCondition} from "src/controllers/process/processDataHandler";
@@ -14,6 +14,7 @@ import TableCell from '@material-ui/core/TableCell';
 import TableContainer from '@material-ui/core/TableContainer';
 import TableHead from '@material-ui/core/TableHead';
 import TableRow from '@material-ui/core/TableRow';
+import TableFooter from '@material-ui/core/TableFooter';
 import Button from "@material-ui/core/Button";
 import Paper from "@material-ui/core/Paper";
 import Typography from "@material-ui/core/Typography";
@@ -26,11 +27,13 @@ import DeleteOutlineIcon from '@material-ui/icons/DeleteOutline';
 import {DialogFormData, DialogFormSubmitFunc} from "../../../../../../components/interface";
 import * as componentsActions from "../../../../../../redux/modules/components/actions";
 import {OutputParamsDef} from "./formDefinition";
+import Radio from '@material-ui/core/Radio';
+import RadioGroup from '@material-ui/core/RadioGroup';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import {AlwaysOutputs} from "../../../../../../constants/editor";
 
 const styles = createStyles({
-    root: {
-
-    },
+    root: {},
     paper: {
         paddingTop: 10,
         paddingBottom: 10,
@@ -41,30 +44,47 @@ const styles = createStyles({
         width: '100%',
         marginBottom: 10,
     },
-    form: {
-    },
+    form: {},
     grey: {
         color: themeColor.grey
     },
     chipAlways: {
         backgroundColor: green[500],
         color: 'white',
+    },
+    selectionContainer: {
+        textAlign: 'center'
     }
 });
 
-export interface Props extends WithStyles<typeof styles>{
+export interface Props extends WithStyles<typeof styles> {
     outputs: Output[],
     onUpdated: (outputs: Output[]) => void,
     openDialogForm: (initValues: any, data: DialogFormData, onSubmit: DialogFormSubmitFunc) => void,
 }
 
-class StepOutputsPane extends React.Component<Props, object> {
-    state = {
+interface State {
+    selection: string,
+    tempOutputs?: Output[]
+}
 
+class StepOutputsPane extends React.Component<Props, object> {
+    state: State = {
+        selection: 'always',
     };
 
     componentDidMount(): void {
-
+        const {outputs} = this.props;
+        if (outputs.length === 1 && outputs[0]?.condition?.operator === 'always') {
+            this.setState({
+                selection: 'always',
+            });
+            this.props.onUpdated([...AlwaysOutputs]);
+        } else {
+            this.setState({
+                selection: 'conditional'
+            });
+        }
     }
 
     getConditionCell = (condition?: OutputCondition) => {
@@ -126,63 +146,93 @@ class StepOutputsPane extends React.Component<Props, object> {
         );
     };
 
+    handleRadioChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        const selection = event.target.value;
+        this.setState({selection});
+        const {outputs} = this.props;
+        if (selection === 'always') {
+            if (outputs.length > 0) {
+                this.setState({tempOutputs: outputs});
+            }
+            this.props.onUpdated([...AlwaysOutputs]);
+        } else {
+            const {tempOutputs} = this.state;
+            if (!!tempOutputs && tempOutputs.length > 0) {
+                this.setState({tempOutputs: undefined});
+                this.props.onUpdated([...tempOutputs]);
+            } else {
+                this.props.onUpdated([]);
+            }
+        }
+    }
+
     render() {
         const {classes, outputs} = this.props;
+        const {selection} = this.state;
         return (
             <div className={classes.root}>
-                <Paper className={classes.paper}>
-                    <table className={classes.headerTable}>
-                        <tbody>
-                        <tr>
-                            <td>
-                                <Typography variant={"subtitle1"}>Outputs</Typography>
-                            </td>
-                            <td align={"right"}>
-                                <Button variant={"outlined"} onClick={this.handleAddClick}><AddIcon/>&nbsp;Add</Button>
-                            </td>
-                        </tr>
-                        </tbody>
-                    </table>
-                    <div>
-                        <TableContainer component={Paper}>
-                            <Table>
-                                <TableHead>
-                                    <TableRow>
-                                        <TableCell>Name</TableCell>
-                                        <TableCell>Condition</TableCell>
-                                        <TableCell align={"right"}>Action</TableCell>
+                <div className={classes.selectionContainer}>
+                    <RadioGroup
+                        row
+                        name="selection"
+                        value={selection}
+                        onChange={this.handleRadioChange}
+                    >
+                        <FormControlLabel value="always" control={<Radio color={"primary"}/>} label="Always"/>
+                        <FormControlLabel value="conditional" control={<Radio color={"secondary"}/>} label="Conditional"/>
+                    </RadioGroup>
+                </div>
+                {selection === 'conditional' &&
+                <TableContainer component={Paper}>
+                    <Table>
+                        <TableHead>
+                            <TableRow>
+                                <TableCell>Name</TableCell>
+                                <TableCell>Condition</TableCell>
+                                <TableCell align={"right"}>Action</TableCell>
+                            </TableRow>
+                        </TableHead>
+                        <TableBody>
+                            {outputs.map((output, i) => {
+                                return (
+                                    <TableRow key={i}>
+                                        <TableCell>{output.name}</TableCell>
+                                        <TableCell>{this.getConditionCell(output.condition)}</TableCell>
+                                        <TableCell align={"right"}>
+                                            <IconButton
+                                                size={"small"}
+                                                color={"primary"}
+                                                onClick={this.handleEditButtonClick(output, i)}
+                                            >
+                                                <EditIcon fontSize={"small"}/>
+                                            </IconButton>
+                                            <IconButton
+                                                size={"small"}
+                                                color={"secondary"}
+                                                onClick={this.handleDeleteButtonClick(i)}
+                                            >
+                                                <DeleteOutlineIcon fontSize={"small"}/>
+                                            </IconButton>
+                                        </TableCell>
                                     </TableRow>
-                                </TableHead>
-                                <TableBody>
-                                    {outputs.map((output, i) => {
-                                        return (
-                                            <TableRow key={i}>
-                                                <TableCell>{output.name}</TableCell>
-                                                <TableCell>{this.getConditionCell(output.condition)}</TableCell>
-                                                <TableCell align={"right"}>
-                                                    <IconButton
-                                                        size={"small"}
-                                                        color={"primary"}
-                                                        onClick={this.handleEditButtonClick(output, i)}
-                                                    >
-                                                        <EditIcon fontSize={"small"}/>
-                                                    </IconButton>
-                                                    <IconButton
-                                                        size={"small"}
-                                                        color={"secondary"}
-                                                        onClick={this.handleDeleteButtonClick(i)}
-                                                    >
-                                                        <DeleteOutlineIcon fontSize={"small"}/>
-                                                    </IconButton>
-                                                </TableCell>
-                                            </TableRow>
-                                        )
-                                    })}
-                                </TableBody>
-                            </Table>
-                        </TableContainer>
-                    </div>
-                </Paper>
+                                )
+                            })}
+                        </TableBody>
+                        <TableFooter>
+                            <TableRow>
+                                <TableCell colSpan={3} align={"center"}>
+                                    <Button
+                                        variant={"text"}
+                                        color={"primary"}
+                                        onClick={this.handleAddClick}
+                                        size={"small"}
+                                    ><AddIcon/>&nbsp;Add Output</Button>
+                                </TableCell>
+                            </TableRow>
+                        </TableFooter>
+                    </Table>
+                </TableContainer>
+                }
             </div>
         )
     }
